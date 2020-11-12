c8033dac7e517ea553d81ba0d0b001d841e320b8
Euan Gordon <ejg9@hw.ac.uk>
2020-11-12 14:50:32 +0000

    Confussion DT

--- Decision_Trees/J48_DT.py
+++ Decision_Trees/J48_DT.py
@@ -1,5 +1,6 @@
 from sklearn import tree as sk
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, cross_val_predict
+from sklearn import metrics
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -8,13 +9,15 @@ plt.rcParams["figure.figsize"] = (30, 20)
 
 data = pd.read_csv("../training_data/x_train_gr_smpl.csv")
 labels = pd.read_csv("../training_data/y_train_smpl.csv")
+X_test = pd.read_csv("../testing_data/x_test_gr_smpl.csv")
+Y_test= pd.read_csv("../testing_data/y_test_smpl.csv")
 
 
 # generate j48 decision tree for various depths and collect average scores
 def decisionTree():
     z = []
-    for i in range(2, 6):
-        print("Tree Depth : " + str(i))
+    for i in range(2, 10):
+        print("\nTree Depth : " + str(i))
         tree = sk.DecisionTreeClassifier(max_depth=i, random_state=42).fit(data, labels)
         visualiseTree(tree.fit(data, labels), depth=i)
         print("Starting CV with tree depth : " + str(i) + " .......")
@@ -24,6 +27,8 @@ def decisionTree():
         print("\n### Mean Score for Tree Depth " + str(i) + " : " + str(tree_scores.mean()) + " ###")
         print("### STD Score for Tree Depth " + str(i) + " : " + str(tree_scores.std()) + " ###\n")
         z.append(tree_scores)
+        cross_pred = cross_val_predict(tree, data, labels, cv=10)
+        print("Confusion Matrix:\n", metrics.confusion_matrix(labels, cross_pred))
     print(z)
 
 
