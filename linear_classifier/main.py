2d3b4f372cc286a9c2766bd3cbdded7f8f500f1b
yaseenjamal <63450896+yaseenjamal@users.noreply.github.com>
2020-11-12 15:08:22 +0000

    Redo linear classifier

--- linear_classifier/main.py
+++ linear_classifier/main.py
@@ -1,21 +1,18 @@
-from sklearn.linear_model import LinearRegression
 import pandas
 import numpy
-from sklearn.model_selection import cross_val_score
-
-
-def display_scores(scores):
-    print("Scores:", scores)
-    print("Mean:", scores.mean())
-    print("Standard deviation:", scores.std())
-
-
-linear_classifier = LinearRegression()
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import confusion_matrix
+from sklearn.linear_model import SGDClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
 
 training_data = pandas.read_csv("../training_data/x_train_gr_smpl.csv")
 labels = pandas.read_csv("../training_data/y_train_smpl.csv")
+labels = numpy.ravel(labels)
 
+# Always scale the input. The most convenient way is to use a pipeline.
+linear_classifier = make_pipeline(StandardScaler(), SGDClassifier(max_iter=1000, tol=1e-3))
 linear_classifier.fit(training_data, labels)
-
-linear_scores = cross_val_score(linear_classifier, training_data, labels, scoring="neg_mean_squared_error", cv=10)
-display_scores(numpy.sqrt(-linear_scores))
+label_predictions = cross_val_predict(linear_classifier, training_data, labels, cv=10)
+confusion_matrix = confusion_matrix(labels, label_predictions)
+print(confusion_matrix)
