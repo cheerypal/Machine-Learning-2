3faea0dca591b1a57aa50153174dd0c146da6aba
amaan-akram <amaan7akram@gmail.com>
2020-11-17 14:55:42 +0000

    move 4000 & 9000 implementation

---
+++ move4000.py
@@ -0,0 +1,18 @@
+import pandas
+import os
+
+os.makedirs("4000_data", exist_ok=True)
+
+#CHANGE THESE VARIABLES TO PICK WHAT FILE YOU WANT TO MOVE DATA FROM AND TO
+TRAIN_FILE_NAME = "y_train_smpl_0.csv"  #MOVE DATA FROM THIS FILE
+TEST_FILE_NAME = "y_test_smpl_0.csv"    #TO THIS FILE
+
+training_file = pandas.read_csv("training_data/" + TRAIN_FILE_NAME)
+testing_file = pandas.read_csv("testing_data/" + TEST_FILE_NAME)
+
+
+training_file_4000 = training_file[3999:]
+testing_file_4000 = testing_file.append(training_file[:4001])
+
+training_file_4000.to_csv("4000_data/" + TRAIN_FILE_NAME + "_4000.csv", header=None, index=False)
+testing_file_4000.to_csv("4000_data/" + TEST_FILE_NAME + "_4000.csv", header=None, index=False)
